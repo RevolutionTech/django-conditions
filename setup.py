92fe793a6871318d5bcc575cf892d90483785432
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-03-08 18:32:46 -0800

    Update VERSION

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ requirements = [
 
 setup(
     name='django-conditions',
-    version='0.9.13',
+    version='0.9.14.1',
     packages=find_packages(),
     include_package_data=True,
     license='ISC License',
